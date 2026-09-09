6242029303dc079dc00bd1b8ac7cac61f42a195a
Rahul Mishra <rahulmishra01512@gmail.com>
2026-09-10 01:05:50 +0530

    Clone Demo

--- Collections/src/ListDemo.java
+++ Collections/src/ListDemo.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListDemo {
+    public static void main(String[] args) {
+        List<Integer> l1 = new ArrayList<>();
+        l1.add(10);
+        l1.add(20);
+        l1.add(30);
+        l1.add(40);
+        l1.add(50);
+        for (int ele : l1){ //to print element
+            System.out.print(ele +" ");
+        }
+        System.out.println();//for space
+        //Or
+        System.out.println(l1); //for List
+
+    }
 }
